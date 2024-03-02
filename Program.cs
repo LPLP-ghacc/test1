b0c98ef51c47c5b678d7bb8ee3f689910449461b
LPLP <lilpophill@gmail.com>
2024-03-02 19:46:56 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        double a = 377;
+        double b = 120;
+
+        double c = a / b;
+
+        Console.WriteLine(c);
+    }
+}
